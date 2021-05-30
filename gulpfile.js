6eddc9859885e8d3bbd640dd3ef930944f385288
Muhammad Fauzan <developerfauzan@gmail.com>
2021-05-31 04:45:42 +0700

    fix: #5

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const { src, dest } = require("gulp");
 const minify = require("gulp-minify");
 
 function minifyjs() {
-  return src("test/adrest.js", { allowEmpty: true })
+  return src("src/adrest.js", { allowEmpty: true })
     .pipe(minify({ noSource: true }))
     .pipe(dest("dist"));
 }
--- src/adrest.js
+++ src/adrest.js
@@ -1,2 +1,65 @@
-import "./include-tags.js";
-import "./extends.js";
+let tags = document.getElementsByTagName("include");
+let extendTags = document.getElementsByTagName("extends");
+let app = document.getElementById("adrest");
+let fetchChain = [];
+
+for (let tag of tags) {
+  let src = tag.attributes["src"] ? tag.attributes["src"].value : null;
+
+  if (src) {
+    fetchChain.push(
+      new Promise((resolve, reject) => {
+        fetch(src)
+          .then((response) => response.text())
+          .then((html) => {
+            resolve(html);
+          })
+          .catch((error) => {
+            reject(error);
+          });
+      })
+    );
+  } else {
+    let oldContent = tag.innerHTML;
+    tag.innerHTML = "";
+    fetchChain.push(
+      new Promise((resolve, reject) => {
+        resolve(oldContent);
+      })
+    );
+  }
+}
+
+Promise.all(fetchChain).then((values) => {
+  for (value of values) !app || app.insertAdjacentHTML("beforebegin", value);
+});
+
+for (let tag of extendTags) {
+  let src = tag.attributes["src"] ? tag.attributes["src"].value : null;
+  if (src) {
+    fetch(src)
+      .then((response) => response.text())
+      .then((html) => {
+        let htmlElement = document.createElement("html");
+
+        let htmlReset = htmlElement;
+        htmlReset.innerHTML = html.outerHTML;
+
+        let lastHtml = htmlElement;
+        lastHtml.innerHTML = htmlReset.innerHTML;
+        document.head.innerHTML =
+          lastHtml.getElementsByTagName("head")[0].innerHTML;
+        tag.innerHTML = lastHtml.getElementsByTagName("body")[0].innerHTML;
+
+        let fragment = document.createDocumentFragment();
+        while (tag.firstChild) {
+          fragment.appendChild(tag.firstChild);
+        }
+
+        tag.parentNode.replaceChild(fragment, tag);
+      })
+      .catch((error) => {
+        console.warn(error);
+      });
+  }
+}
